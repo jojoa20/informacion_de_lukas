ebeb013f4e6790c5f07f8dde986d15365b084afc
Leonardo José Támara Bolívar <leonardobolivar2005@gmail.com>
2026-03-15 19:08:36 -0500

    feat(ui): remove redundant ProblemStatement section to focus on LeakBuster cinematic story

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,6 @@
 import React, { useEffect } from "react";
 import Navbar from "@/components/Navbar";
 import Hero from "@/components/Hero";
-import ProblemStatement from "@/components/ProblemStatement";
 import InputMethods from "@/components/InputMethods";
 import LeakBuster from "@/components/LeakBuster";
 import LeakDetection from "@/components/LeakDetection";
@@ -43,7 +42,6 @@ export default function Home() {
       <Navbar />
       <BackgroundNetwork />
       <Hero />
-      <ProblemStatement />
       <LeakBuster />
       <InputMethods />
       <LeakDetection />
